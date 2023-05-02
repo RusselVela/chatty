f21ad05b39a671219c47cebee135770406496846
Russel Vela <vela.russel@gmail.com>
2023-05-02 06:20:37 -0600

    feat(sync): Added mutex to write operation to avoid race conditions

--- internal/app/datasourcce/repository/inmemory/tokenToUser.go
+++ internal/app/datasourcce/repository/inmemory/tokenToUser.go
@@ -1,13 +1,18 @@
 package inmemory
 
+import "sync"
+
 type tokenToUsersTable map[string]string
 
 var tokenToUser tokenToUsersTable
+var tokenMu sync.Mutex
 
 // AddTokenToUser adds a new pair of [user, token] to this table.
 // When the token is used to authenticate the websocket connection, the entry is deleted.
 func AddTokenToUser(userId string, token string) {
+	tokenMu.Lock()
 	tokenToUser[userId] = token
+	tokenMu.Unlock()
 }
 
 // GetToken retrieves the token associated with the userId, if any.
@@ -17,5 +22,7 @@ func GetToken(userId string) string {
 
 // DeleteTokenToUser removes the [userId, token] pair from the table
 func DeleteTokenToUser(userId string) {
+	tokenMu.Lock()
 	delete(tokenToUser, userId)
+	tokenMu.Unlock()
 }
